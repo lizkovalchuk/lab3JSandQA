52e880158f0ceef7a0d483f5b4b926959c3fe03c
Liz Kovalchuk <liz.kovalchuk@gmail.com>
2018-02-12 10:12:16 -0500

    function to test student ID

--- Lab-3-unittest.js
+++ Lab-3-unittest.js
@@ -5,3 +5,16 @@ alert("lab 3");//please delete this line once connected.
  * Returns true if input validates.
  * @param {string} idIn
  */
+
+//function to check for valid student number
+
+function checkHumbrId(value) {
+    "use strict";
+
+    var IDRegEx = new RegEx("/(n|N)d{8}/");
+    if (IDRegEx.test(value)) {
+        return true;
+    } else {
+        return false;
+    }
+}
