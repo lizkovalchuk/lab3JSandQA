5b8a29a6e22d6c670a41c3a921b35cf26d018c24
Liz Kovalchuk <liz.kovalchuk@gmail.com>
2018-02-12 09:37:40 -0500

    the files

---
+++ Lab-3-unittest.js
@@ -0,0 +1,7 @@
+//LAB 3 - UNIT TESTING
+alert("lab 3");//please delete this line once connected.
+/**
+ * Validate input is a Humber College Student number.
+ * Returns true if input validates.
+ * @param {string} idIn
+ */
