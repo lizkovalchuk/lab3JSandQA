8a94b02868bb64143781067e3c1af95db8085ea0
Jessica Wong <jessica.pearl.wong@gmail.com>
2018-02-12 10:14:30 -0500

    added unittest

--- Lab-3-unittest.js
+++ Lab-3-unittest.js
@@ -5,3 +5,30 @@ alert("lab 3");//please delete this line once connected.
  * Returns true if input validates.
  * @param {string} idIn
  */
+
+function test_checkHumberId(valueIn, expected) {
+
+    // Create a new element, and append to the output
+    var elem = document.createElement("span");
+    elem.setAttribute("id", "passFail");
+    output.appendChild(elem);
+    var passFailOut = getElementById("passFail");
+
+    var result = checkHumberId(valueIn);
+    var passOrFail = passOrFail(result);
+    var output = document.getElementById("data");
+
+    function passOrFail(res) {
+        if (res) {
+            passFailOut.innerHTML = "==PASSED==";
+            passFailOut.style.color = "green";
+        }
+        else {
+            passFailOut.innerHTML = "xxFAILEDxx";
+            passFailOut.style.color = "green";
+        }
+    }
+
+    var mssg = "Value Tested: " + valueIn + " | Expected Result: " + expected + " " + passOrFail;
+    output.innerHTML += mssg;
+}
